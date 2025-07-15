4a64bb2f1331b28f3a721f38be84812e7bcd1ac5
aaron-demelo <aarondemelo@gmail.com>
2025-07-15 17:05:56 +0530

    code cleanup

--- Sources/BuilderIO/Components/BuilderBlock.swift
+++ Sources/BuilderIO/Components/BuilderBlock.swift
@@ -2,8 +2,6 @@ import SwiftUI
 
 //BuilderBlock forms the out layout container for all components mimicking Blocks from response. As blocks can have layout direction of either horizontal or vertical a check is made and layout selected.
 
-//BuilderBlock forms the out layout container for all components mimicking Blocks from response. As blocks can have layout direction of either horizontal or vertical a check is made and layout selected.
-
 enum BuilderLayoutDirection {
   case horizontal
   case vertical
